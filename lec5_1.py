1dd757c8b676b5048acd167455de17dfa824d6f5
01201201 <gimyoung04@gmail.com>
2023-10-03 23:16:25 +0900

    update

--- lec5_1.py
+++ lec5_1.py
@@ -1,16 +1,16 @@
 # 문자열을 날짜로 변환 
-""" from datetime import datetime as dt
+from datetime import datetime as dt
 print(dt.now())
 date_string = '2021-07-08'
 date_object = dt.strptime(date_string, '%Y-%m-%d')
-print(date_object) """
+print(date_object)
 
 #날짜를 문자열로 변환
-""" date_object = dt.now()
+date_object = dt.now()
 date_string = date_object.strftime('%Y-%m-%d')
-print(date_string) """
+print(date_string)
 
-""" import mod.utils as mu
+import mod.utils as mu
 dtnow = mu.get_dtnow()
 print(dtnow)
 
@@ -18,7 +18,7 @@ ret = mu.cvt_time2str("2023-09-25")
 print(ret)
 
 res = mu.cvt_str2time()
-print(res) """
+print(res)
 # os: 운영체제와 상호작용하기 위한 함수 제공
 
 """ import os
@@ -39,7 +39,7 @@ print(os.listdir())
 os.mkdir('new_directory')
 print(os.listdir()) """
 
-import mod.utils as mu
+""" import mod.utils as mu
 import os
 print(mu.get_curdir())
 
@@ -51,7 +51,7 @@ print(os.listdir())
 
 import sys
 print(sys.version)
-print(sys.argv)
+print(sys.argv) """
 
 # os 모듈
 """ import mod.utils as mu
